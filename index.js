82c177c8a737104a379599114847a7019727e503
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-05-27 21:23:46 +0200

    Optimizado el codigo con recomendaciones Mario

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-const apellido1 = "González";
-const apellido2 = "Gonzzali";
-const apellido3 = "Masserati";
-const apellido4 = "Baguette";
-const apellido5 = "Piccolini";
+const apellidos = [
+  "González",
+  "Gonzzali",
+  "Masserati",
+  "Baguette",
+  "Piccolini",
+];
 
-function esItaliano(palabra) {
-  if (palabra[palabra.length - 1] === "i") {
-    return false;
-  } else {
-    return true;
-  }
-}
+const esItaliano = (palabra) => palabra[palabra.length - 1] === "i";
 
 function respuestaEsItaliano(esItaliano, apellido) {
-  if (esItaliano(apellido)) {
-    console.log(
-      `${apellido}... Este apellido podría ser de cualquier país. No crees?`
-    );
-  } else {
-    console.log(
-      `${apellido}... Este apellido tiene muchas probabilidades de ser italiano. No crees?`
-    );
-  }
+  console.log(
+    esItaliano(apellido)
+      ? `${apellido}... Este apellido podría ser de cualquier país. No crees?`
+      : `${apellido}... Este apellido tiene muchas probabilidades de ser italiano. No crees?`
+  );
 }
 
-respuestaEsItaliano(esItaliano, apellido1);
-respuestaEsItaliano(esItaliano, apellido2);
-respuestaEsItaliano(esItaliano, apellido3);
-respuestaEsItaliano(esItaliano, apellido4);
-respuestaEsItaliano(esItaliano, apellido5);
+apellidos.forEach((element) => respuestaEsItaliano(esItaliano, element));
